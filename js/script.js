585d2973523ba506e7a7417b90b9807060b96f0c
Danko-M <dankomilutinovic@gmail.com>
2015-08-19 21:54:48 +0200

    added animated search

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.fa-search').on('click', function(){
+		if ($(window).width() < 768) {
+			$('.user-area-bg').toggleClass('new-height');
+		}
+		$('.user-area input').animate({width:'toggle'},150);
+	});
+
+});

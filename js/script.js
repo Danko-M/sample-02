234c1a231611c7f88a68c9b6fc956a7b56c2e5b8
Danko-M <dankomilutinovic@gmail.com>
2015-08-20 12:30:25 +0200

    added widgets

--- js/script.js
+++ js/script.js
@@ -6,4 +6,7 @@ $(function(){
 		$('.user-area input').animate({width:'toggle'},100).focus();
 	});
 
+	$('a').not('#flickr_badge_wrapper a').on('click', function(e){
+		e.preventDefault();
+	});
 });
